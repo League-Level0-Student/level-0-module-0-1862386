fb48d117a36bbc3e014d8343b71cbeaef426f9d5
1862386 <league@WTS-IM-17.attlocal.net>
2018-05-03 19:59:51 -0700

    KnockKnock, Greeter, Robot Square

---
+++ src/Greeter.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	
+	 String rob = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hi "+ rob );
+	
+}
+}
---
+++ src/RobotGraffiti.java
@@ -0,0 +1,6 @@
+
+public class RobotGraffiti {
+public static void main(String[] args) {
+	
+}
+}
